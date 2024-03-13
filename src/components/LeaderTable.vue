6806a1ea2052dde91d8342047f4af7e7747d3927
Zafar Qalandarov <zqalandarov@texnomart.uz>
2024-03-13 22:09:50 +0500

    asd

--- src/components/LeaderTable.vue
+++ src/components/LeaderTable.vue
@@ -5,6 +5,52 @@ import BaseImg from "src/components/UI/BaseImg.vue";
 <template>
   <div class="leaders-list-home">
     <div>
+      <div class="three-rating-box">
+        <div class="three-rating__item">
+          <div class="position-number">2</div>
+          <div class="profile-circle">
+            <img
+              class="profile-img"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_1bpO0XDD8fbmRvnbnkCoQNFFoH3AqofVTg&usqp=CAU"
+              alt=""
+            />
+          </div>
+          <div class="text-center">
+            <div class="text-username">@khasan</div>
+            <div>4542 pts</div>
+          </div>
+        </div>
+        <div class="three-rating__item">
+          <div class="position-number">1</div>
+          <img class="crown-img" src="/images/icons/crown.png" alt="" />
+          <div class="profile-circle">
+            <img
+              class="profile-img"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT85_hgbSWovX-31r0_mHGwL8_HO4-2-Y-J4Z3cUjzEvF2YIm9ILGgoRS_lAEnmZrjQm30&usqp=CAU"
+              alt=""
+            />
+          </div>
+          <div class="text-center">
+            <div class="text-username">@zafaric</div>
+            <div>4542 pts</div>
+          </div>
+        </div>
+        <div class="three-rating__item">
+          <div class="position-number">3</div>
+          <div class="profile-circle">
+            <img
+              class="profile-img"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu_r1Z91AaWpSXXoHKASTdGIwqU43_-O8niWO9Cw8uYTJnfksBq6YPjM4SaxMMzNlSza0&usqp=CAU"
+              alt=""
+            />
+          </div>
+          <div class="text-center">
+            <div class="text-username">@abbos</div>
+            <div>4542 pts</div>
+          </div>
+        </div>
+      </div>
+
       <table>
         <thead>
           <th>#</th>
@@ -74,6 +120,58 @@ import BaseImg from "src/components/UI/BaseImg.vue";
 </template>
 
 <style lang="scss">
+.three-rating-box {
+  display: flex;
+  margin-top: 100px;
+  margin-bottom: 24px;
+  .three-rating__item {
+    width: calc(100% / 3);
+    position: relative;
+    z-index: 1;
+    .text-username {
+      color: $primary;
+      margin-top: 5px;
+    }
+    .position-number {
+      font-size: 20px;
+      text-align: center;
+    }
+
+    .crown-img {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+      top: -45px;
+      width: 60px;
+    }
+    .profile-circle {
+      position: relative;
+      z-index: 2;
+      width: 100%;
+      height: calc((100vw - 24px) / 3);
+      border-radius: 100%;
+      border: 4px solid $gray-5;
+      // overflow: hidden;
+      background-color: #fff;
+      img.profile-img {
+        width: 100%;
+        object-fit: contain;
+        border-radius: 100%;
+        height: 100%;
+      }
+    }
+  }
+  .three-rating__item:nth-child(2) {
+    z-index: 2;
+    .position-number {
+      transform: translateY(-80px);
+      font-weight: 700;
+    }
+    .profile-circle {
+      transform: scale(1.3) translateY(-20px);
+    }
+  }
+}
 .leaders-list-home {
   table {
     width: 100%;
