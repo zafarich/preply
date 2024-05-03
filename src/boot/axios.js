43f37d0ed38ddea7cf7fb0d4d2aa9710b6c33ad5
Fazliddin <fxakimov020@gmail.com>
2024-05-03 19:09:41 +0500

    updated

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,67 +1,75 @@
-import { boot } from "quasar/wrappers";
-import axios from "axios";
-import { getTokenFromCache } from "src/utils/auth";
-import { Notify } from "quasar";
-import { getServerError } from "src/utils/helpers";
-import { useUserStore } from "src/stores/user";
+import { boot } from 'quasar/wrappers'
+import axios from 'axios'
+import { getTokenFromCache } from 'src/utils/auth'
+import { getServerError } from 'src/utils/helpers'
+import { useUserStore } from 'src/stores/user'
 
-const api = axios.create({ baseURL: process.env.BASE_URL });
+const api = axios.create({ baseURL: process.env.BASE_URL })
 
 export default boot(({ app }) => {
-  const userStore = useUserStore();
-  api.interceptors.request.use(
-    (config) => {
-      const token =
-        getTokenFromCache() ||
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE0ODM4MTk3LCJpYXQiOjE3MTQ2NjUzOTcsImp0aSI6ImU5OTgwZGRjOWM3NDRkZGJiYzdiNGE5ZmM5YjQ3N2E5IiwidXNlcl9pZCI6M30.Zyrlwg786eBMLalWMCVAm1f4AjirPKWgqG4JVSfcyeI";
-      if (token) config.headers.Authorization = "Bearer " + token;
+    const userStore = useUserStore()
+    api.interceptors.request.use(
+        (config) => {
+            const token =
+                getTokenFromCache() ||
+                'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE0ODM4MTk3LCJpYXQiOjE3MTQ2NjUzOTcsImp0aSI6ImU5OTgwZGRjOWM3NDRkZGJiYzdiNGE5ZmM5YjQ3N2E5IiwidXNlcl9pZCI6M30.Zyrlwg786eBMLalWMCVAm1f4AjirPKWgqG4JVSfcyeI'
+            if (token) config.headers.Authorization = 'Bearer ' + token
 
-      let lang = localStorage.getItem("locale");
+            let lang = localStorage.getItem('locale')
 
-      if (lang?.startsWith("uz")) lang = "uz";
+            if (lang?.startsWith('uz')) lang = 'uz'
 
-      return config;
-    },
-    (error) => Promise.reject(error)
-  );
+            // if (lang) config.headers['Language'] = lang
 
-  api.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    async (error) => {
-      let message = getServerError(error, "errorMessage");
-      const status = error?.response?.status;
-      if ("pass" in error?.config) {
-        return Promise.reject(error);
-      }
+            // config.headers['Access-Control-Allow-Origin'] = '*'
+            // config.headers['Access-Control-Allow-Credentials'] = true
+            // config.headers['Access-Control-Allow-Methods'] =
+            //     'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+            // config.headers['Content-Type'] = 'application/x-www-form-urlencoded'
 
-      if (status === 401) {
-        return { data: { result: null, error: true } };
-      } else if (status?.toString()?.slice(0, 1) === 5) {
-        message = "Internal Server Error";
-      } else if (status === 405) {
-        message = "Method Not Allowed";
-      } else if (status === 404) {
-        message = "Not Found";
-      } else if (status === 403) {
-        message = "403 Forbidden";
-      } else {
-        message = message ?? "Error 500. Backendda nomalum xatolik yuz berdi!";
-      }
+            return config
+        },
+        (error) => Promise.reject(error),
+    )
 
-      // if (message) {
-      //   Notify.create({
-      //     progress: true,
-      //     position: "top",
-      //     message,
-      //     type: "negative",
-      //     timeout: 4000,
-      //   });
-      // }
-      return { data: { result: null, error: true } };
-    }
-  );
-});
+    api.interceptors.response.use(
+        (response) => {
+            return response
+        },
+        async (error) => {
+            let message = getServerError(error, 'errorMessage')
+            const status = error?.response?.status
+            if ('pass' in error?.config) {
+                return Promise.reject(error)
+            }
 
-export { api };
+            if (status === 401) {
+                return { data: { result: null, error: true } }
+            } else if (status?.toString()?.slice(0, 1) === 5) {
+                message = 'Internal Server Error'
+            } else if (status === 405) {
+                message = 'Method Not Allowed'
+            } else if (status === 404) {
+                message = 'Not Found'
+            } else if (status === 403) {
+                message = '403 Forbidden'
+            } else {
+                message =
+                    message ?? 'Error 500. Backendda nomalum xatolik yuz berdi!'
+            }
+
+            // if (message) {
+            //   Notify.create({
+            //     progress: true,
+            //     position: "top",
+            //     message,
+            //     type: "negative",
+            //     timeout: 4000,
+            //   });
+            // }
+            return { data: { result: null, error: true } }
+        },
+    )
+})
+
+export { api }
