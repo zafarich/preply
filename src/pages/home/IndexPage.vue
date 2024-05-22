62b6320e0afbbbc8714b9ec59f3d926f9213de5f
Fazliddin <fxakimov020@gmail.com>
2024-05-22 22:42:15 +0500

    updated

--- src/pages/home/IndexPage.vue
+++ src/pages/home/IndexPage.vue
@@ -6,13 +6,17 @@ import TestTypes from './sections/TestTypes.vue'
 import PopularScience from './sections/PopularScience.vue'
 import TestsList from 'src/components/TestsList.vue'
 import LeadersList from './sections/LeadersList.vue'
-import { getTokenFromCache, setTokenToCache } from 'src/utils/auth'
+import {
+    getTokenFromCache,
+    removeTokenFromCache,
+    setTokenToCache,
+} from 'src/utils/auth'
 import VueJwtDecode from 'vue-jwt-decode'
 import { useUserStore } from 'src/stores/user'
 import { useMainStore } from 'src/stores/main'
 
 import { useReferencesStore } from 'src/stores/references'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 
 const { t } = useI18n()
 const userStore = useUserStore()
@@ -24,28 +28,29 @@ const subjects = ref([])
 const banners = ref([])
 
 const route = useRoute()
-
-// console.log('token1', token)
-// http://localhost:8080/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE2MzUxODkzLCJpYXQiOjE3MTYxNzkwOTMsImp0aSI6ImJkYzI3ZTg1Y2ZiZjRmNzhiZmI5M2RkMTc3ZTNiNmJiIiwidXNlcl9pZCI6NCwidXNlcl9kYXRhIjp7ImlkIjo0LCJmaXJzdF9uYW1lIjoiIiwibGFzdF9uYW1lIjoiIiwidGVsZWdyYW1fdXNlcm5hbWUiOiJpa2hhc2FubXVzYWV2IiwiZnVsbF9uYW1lIjoiaGFzYW4iLCJsYW5ndWFnZSI6InV6IiwicGhvbmUiOiIrOTk4OTAwMzExMTk5IiwiaXNfcHJlbWl1bSI6ZmFsc2UsImlzX2ZyZWVfYXR0ZW1wdHNfbGVmdCI6dHJ1ZSwiY2FyZHMiOltdLCJyZWdpb24iOiJRb3JhcWFscG9nJ2lzdG9uIn19.t9l0wHFUBd731j7vTAeav56Rrv9e6xOEitiJV35J9Rg&refresh_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTcxNzA0MzA5MywiaWF0IjoxNzE2MTc5MDkzLCJqdGkiOiJjNWEyYzkxMTQ5YjA0YTA3Yjg4Y2FmY2EyMGE2ZmFjOSIsInVzZXJfaWQiOjQsInVzZXJfZGF0YSI6eyJpZCI6NCwiZmlyc3RfbmFtZSI6IiIsImxhc3RfbmFtZSI6IiIsInRlbGVncmFtX3VzZXJuYW1lIjoiaWtoYXNhbm11c2FldiIsImZ1bGxfbmFtZSI6Imhhc2FuIiwibGFuZ3VhZ2UiOiJ1eiIsInBob25lIjoiKzk5ODkwMDMxMTE5OSIsImlzX3ByZW1pdW0iOmZhbHNlLCJpc19mcmVlX2F0dGVtcHRzX2xlZnQiOnRydWUsImNhcmRzIjpbXSwicmVnaW9uIjoiUW9yYXFhbHBvZydpc3RvbiJ9fQ.FvTl5PV_hPRcsb2tG4dGcVVB_LOqZ9795tLRuIQ2VVI
+const router = useRouter()
 
 onMounted(() => {
-    let token = getTokenFromCache()
+    removeTokenFromCache()
 
-    // console.log('token', token)
-    console.log('token1', token)
-    console.log('route.query.access_token', route.query.access_token)
-
-    if (!token || token == 'undefined' || token == undefined) {
-        token = route.query.access_token
-
-        console.log('token2', token)
-        setTokenToCache(token)
-        console.log('getTokenFromCache', getTokenFromCache())
-        let decoded = VueJwtDecode.decode(token)
-        userStore.updateUserData({ ...decoded.user_data })
-        mainStore.setLanguage(decoded.user_data.language)
-        console.log('decoded', decoded)
+    // let token = getTokenFromCache()
+    if (!route.query.access_token) {
+        router.push({
+            name: 'home',
+            query: {
+                access_token:
+                    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE2NTcxNzM0LCJpYXQiOjE3MTYzOTg5MzQsImp0aSI6IjY5YjhlNmJiMzU1ZTQyOGU5YWNhYzM2N2I4MjE2YzM0IiwidXNlcl9pZCI6MSwidXNlcl9kYXRhIjp7ImlkIjoxLCJmaXJzdF9uYW1lIjoiS2hhZiIsImxhc3RfbmFtZSI6Ik11c2FldjMzMzMiLCJ0ZWxlZ3JhbV91c2VybmFtZSI6bnVsbCwiZnVsbF9uYW1lIjpudWxsLCJsYW5ndWFnZSI6InV6IiwicGhvbmUiOiIxIiwiaXNfcHJlbWl1bSI6ZmFsc2UsImlzX2ZyZWVfYXR0ZW1wdHNfbGVmdCI6dHJ1ZSwiY2FyZHMiOlt7ImlkIjoxNywiY2FyZF9udW1iZXIiOiI4NjAwNDkqKioqKio2NDc4IiwiZXhwaXJlIjoiMDMvOTkiLCJ2ZXJpZnkiOnRydWUsInJlY3VycmVudCI6dHJ1ZSwidXNlciI6MSwiY3JlYXRlZCI6IjIwMjQtMDUtMTVUMjM6NDE6MTIuMjUwNDA5KzA1OjAwIn0seyJpZCI6MjYsImNhcmRfbnVtYmVyIjoiODYwMDA2KioqKioqNjMxMSIsImV4cGlyZSI6IjAzLzk5IiwidmVyaWZ5IjpmYWxzZSwicmVjdXJyZW50IjpmYWxzZSwidXNlciI6MSwiY3JlYXRlZCI6IjIwMjQtMDUtMTdUMTQ6NTY6MjAuODk4OTUzKzA1OjAwIn1dLCJyZWdpb24iOiJRb3JhcWFscG9nJ2lzdG9uIn19.EPFXOWAUGrcv5AB4vMdOZnxYHyyNEF4a_cAXOsMGtWg',
+            },
+        })
     }
+
+    let token = route.query.access_token
+
+    setTokenToCache(token)
+    let decoded = VueJwtDecode.decode(token)
+    userStore.updateUserData({ ...decoded.user_data })
+    mainStore.setLanguage(decoded.user_data.language)
+
     fetchData()
 })
 
