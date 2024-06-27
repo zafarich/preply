ed4b150cce68fe6a19ca4743f1d6e4a0d00bd82e
Fazliddin <fxakimov020@gmail.com>
2024-06-27 10:44:13 +0500

    updated qr

--- src/components/common/LangSwitcher.vue
+++ src/components/common/LangSwitcher.vue
@@ -38,7 +38,7 @@ const changeLang = async (lang) => {
     console.log('lang', lang)
     mainStore.setLanguage(lang)
     const language =
-        lang === 'uz-Latn' ? 'uz' : lang === 'uz-Cyrl' ? 'kr' : 'ru'
+        lang === 'uz-Latn' ? 'uz' : lang === 'uz-Cyrl' ? 'qr' : 'ru'
 
     if (getTokenFromCache()) await userStore.updateUser({ language })
 
